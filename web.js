ad16eac1805f9f31e5f722f983bcc82d597c0f3e
Samuel Chang <spamchang@gmail.com>
2013-07-11 04:39:11 +0000

    Debug web.js

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require ('fs');
 var app = express.createServer(express.logger());
+var buf = new Buffer()
 
 app.get('/', function(request, response) {
   response.send(buf.ToString(fs.readFileSync("index.html")));
